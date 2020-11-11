ddb0245b48c808597973e9b9e8dc63dc2236e801
Isaac Venus <isaac.venus@student.kuleuven.be>
2020-11-11 17:03:20 +0100

    automatische detectie van de richting van de persoon + opslaan van de coordinaten zodat je het niet altijd opnieuw moet berekenen

--- Python/knie.py
+++ Python/knie.py
@@ -10,6 +10,8 @@ import cv2
 import os
 import numpy as np
 from sys import platform
+import ntpath
+import json
 
 def angle(a, b, c, d):
     A = np.sqrt((d[0] - c[0])**2 + (d[1] - c[1])**2)
@@ -19,6 +21,37 @@ def angle(a, b, c, d):
     cos_theta = (A**2 + D**2 - B**2)/(2 * A * D)
 
     return np.arccos(cos_theta)
+
+def voorstel(keyp):
+    # check naar welke richting de fietser kijkt
+    if keyp[0][1][0] > keyp[0][8][0]:
+        heup, knie, enkel = 9, 10, 11
+    else:
+        heup, knie, enkel = 12, 13, 14
+    theta_1 = angle([keyp[0][1][0], keyp[0][8][1]], keyp[0][8], keyp[0][8], keyp[0][1])
+    theta_2 = angle(keyp[0][8], keyp[0][1], keyp[0][2], keyp[0][3])
+    theta_3 = angle(keyp[0][heup], keyp[0][knie], keyp[0][knie], keyp[0][enkel])
+    print("-------------------------------")
+    print("Hoek tussen rug en horizontale:")
+    print(np.degrees(theta_1))
+    print("-------------------------------")
+    print("Hoek tussen rug en opperarmbeen:")
+    print(np.degrees(theta_2))
+    print("-------------------------------")
+    print("Hoek van de knie")
+    print(np.degrees(theta_3))
+
+    # Bepalen van de zadelhoogte om de optimale hoek van de knie te bekomen.
+
+    lengte_dijbeen = np.sqrt((keyp[0][heup][0] - keyp[0][knie][0])**2 + (keyp[0][heup][1] - keyp[0][knie][1])**2)
+    lengte_scheenbeen = np.sqrt((keyp[0][knie][0] - keyp[0][enkel][0])**2 + (keyp[0][knie][1] - keyp[0][enkel][1])**2)
+
+    c = np.sqrt(lengte_scheenbeen**2 + lengte_dijbeen**2 - 2*lengte_dijbeen*lengte_scheenbeen*np.cos(np.radians(140)))
+    A_y = keyp[0][enkel][1] - (np.sqrt(c**2-(keyp[0][enkel][0] - keyp[0][heup][0])**2)) 
+    print("-------------------------------")
+    print("Mag ik voorstellen om het zadel te verhogen/verlagen met:")
+    print(keyp[0][8][1] - A_y)
+
 
 try:
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -40,49 +73,34 @@ try:
     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
     params = dict()
     params["model_folder"] = "../../../models/"
-
-    # Starting OpenPose
-    opWrapper = op.WrapperPython()
-    opWrapper.configure(params)
-    opWrapper.start()
-
-    # Process Image
-    datum = op.Datum()
-    imageToProcess = cv2.imread(sys.argv[1])
-    datum.cvInputData = imageToProcess
-    opWrapper.emplaceAndPop([datum])
-    keyp = datum.poseKeypoints
-    theta_1 = angle([keyp[0][1][0], keyp[0][8][1]], keyp[0][8], keyp[0][8], keyp[0][1])
-    theta_2 = angle(keyp[0][8], keyp[0][1], keyp[0][2], keyp[0][3])
-    theta_3 = angle(keyp[0][12], keyp[0][13], keyp[0][13], keyp[0][14])
-    print("-------------------------------")
-    print("Hoek tussen rug en horizontale:")
-    print(np.degrees(theta_1))
-    print("-------------------------------")
-    print("Hoek tussen rug en opperarmbeen:")
-    print(np.degrees(theta_2))
-    print("-------------------------------")
-    print("Hoek van de knie")
-    print(np.degrees(theta_3))
-
-    # Bepalen van de zadelhoogte om de optimale hoek van de knie te bekomen.
-
-    lengte_dijbeen = np.sqrt((keyp[0][12][0] - keyp[0][13][0])**2 + (keyp[0][12][1] - keyp[0][13][1])**2)
-    lengte_scheenbeen = np.sqrt((keyp[0][13][0] - keyp[0][14][0])**2 + (keyp[0][13][1] - keyp[0][14][1])**2)
-
-    c = np.sqrt(lengte_scheenbeen**2 + lengte_dijbeen**2 - 2*lengte_dijbeen*lengte_scheenbeen*np.cos(np.radians(140)))
-    A_y = keyp[0][14][1] - (np.sqrt(c**2-(keyp[0][14][0] - keyp[0][12][0])**2)) 
-    print("-------------------------------")
-    print("Mag ik voorstellen om het zadel te verhogen/verlagen met:")
-    print(keyp[0][8][1] - A_y)
-
-
     
-
-    # Display Image
-    #print("Body keypoints: \n" + str(datum.poseKeypoints))
-    cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
-    cv2.waitKey(0)
+    imagename = ntpath.basename(sys.argv[1])
+    imagename.replace(".png", "")
+    imagename.replace(".jpg", "")
+    imagename.replace(".jpeg", "")
+    # check of je al de positie hebt berekend
+    if os.path.isfile("keypoints_" + imagename + ".txt"):
+        keypoints = np.loadtxt("keypoints_" + imagename + ".txt")
+        keypoints = [keypoints]
+        voorstel(keypoints)
+    else:
+        # Starting OpenPose
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(params)
+        opWrapper.start()
+    
+        # Process Image
+        datum = op.Datum()
+        imageToProcess = cv2.imread(sys.argv[1])
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop([datum])
+        keyp = datum.poseKeypoints
+        np.savetxt("keypoints_" + imagename + ".txt", keyp[0])
+        voorstel(keyp)
+        # Display Image
+        #print("Body keypoints: \n" + str(datum.poseKeypoints))
+        cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
+        cv2.waitKey(0)
 except Exception as e:
     print(e)
     sys.exit(-1)
