367838c05c5910e8dcc873d2feace64bb45db8d1
Stan Vanhecke <stan.vanhecke@student.kuleuven.be>
2020-11-12 17:24:26 +0100

    schouderhoek algoritme

--- Python/knie.py
+++ Python/knie.py
@@ -9,6 +9,7 @@ import sys
 import cv2
 import os
 import numpy as np
+import sympy as sp
 from sys import platform
 import ntpath
 import json
@@ -108,3 +109,85 @@ try:
 except Exception as e:
     print(e)
     sys.exit(-1)
+
+## Algoritme voor wijzigen van de stemlengte mbv schouderhoek
+
+def schouderhoek(H,S,Z):
+
+    #H zijn coördinaten van hand op het stuur
+    #S zijn coördinaten van de schouder
+    #Z zijn coördinaten van het zadel (na eventueel aanpassen van hoogte)
+
+    SZ = np.sqrt((S[0]-Z[0])**2 + (S[1]-Z[1])**2)
+    HS = np.sqrt((S[0]-H[0])**2 + (S[1]-H[1])**2)
+    HZ = np.sqrt((Z[0]-H[0])**2 + (Z[1]-H[1])**2)
+
+    cos_alfa = (SZ**2 + HS**2 - HZ**2)/(2*SZ*HS)
+
+    return np.arccos(cos_alfa)
+
+
+
+def circle_intersection(circle1, circle2):
+    '''
+    @summary: calculates intersection points of two circles
+    @param circle1: tuple(x,y,radius)
+    @param circle2: tuple(x,y,radius)
+    @result: tuple of intersection points (which are (x,y) tuple)
+    '''
+     # return self.circle_intersection_sympy(circle1,circle2)
+    x1, y1, r1 = circle1
+    x2, y2, r2 = circle2
+    # http://stackoverflow.com/a/3349134/798588
+    dx, dy = x2 - x1, y2 - y1
+    d = np.sqrt(dx * dx + dy * dy)
+    if d > r1 + r2:
+        print("#1")
+        return None  # no solutions, the circles are separate
+    if d < abs(r1 - r2):
+        print("#2")
+        return None  # no solutions because one circle is contained within the other
+    if d == 0 and r1 == r2:
+        print("#3")
+        return None  # circles are coincident and there are an infinite number of solutions
+
+    a = (r1 * r1 - r2 * r2 + d * d) / (2 * d)
+    h = np.sqrt(r1 * r1 - a * a)
+    xm = x1 + a * dx / d
+    ym = y1 + a * dy / d
+    xs1 = xm + h * dy / d
+    xs2 = xm - h * dy / d
+    ys1 = ym - h * dx / d
+    ys2 = ym + h * dx / d
+
+    return (xs1, ys1), (xs2, ys2)
+
+
+def wijzig_stemlengte(H,S,Z):
+    # H zijn coördinaten van hand op het stuur
+    # S zijn coördinaten van de schouder
+    # Z zijn coördinaten van het zadel (na eventueel aanpassen van hoogte)
+
+    SZ = np.sqrt((S[0]-Z[0])**2 + (S[1]-Z[1])**2)
+    HS = np.sqrt((S[0]-H[0])**2 + (S[1]-H[1])**2)
+
+    alfa = schouderhoek(H,S,Z)
+    if (np.pi/2) < alfa < (np.pi/2 + 5/180 * np.pi):
+        print("De lengte van de stuurpen is goed")
+    elif alfa < np.pi/2:
+        global a
+        a = 1
+        while alfa < np.pi/2:
+            H[0] -= a
+            snijpunten = circle_intersection((H[0],H[1],HS),(Z[0],Z[1],SZ))
+            #S is nu het snijpunt met de kleinste y-waarde, dus het snijpunt dat het hoogst gelegen is op de foto
+            hoogste_snijpunt = snijpunten[0]
+            if snijpunten[1][1] > snijpunten[0][1]:
+                hoogste_snijpunt = snijpunten[1]
+
+            alfa = schouderhoek(H,hoogste_snijpunt,Z)
+            a += 1
+
+    print(a)
+
+    return a
